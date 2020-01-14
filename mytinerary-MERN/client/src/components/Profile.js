c7d5e42308632154a33c5cf32c82fe1136a338a8
Ahmsanz <adrian.h.m.sanz@gmail.com>
2020-01-14 19:42:05 +0100

    Fixing favourites fetch

--- mytinerary-MERN/client/src/components/Profile.js
+++ mytinerary-MERN/client/src/components/Profile.js
@@ -9,28 +9,33 @@ class Profile extends Component {
     state = {
         users: null,
         favourites: []
-        
+
     }
 
     componentDidMount() {
         if (localStorage.userToken) {
         this.getUserByToken()
+
         }
+
     }
 
+
     getUserByToken() {
-        
+
         let token = localStorage.userToken
-        
+
         let decoded = jwt_decode(token);
 
-        
-        
+
+
         axios.get('http://localhost:5000/users/' + decoded._id)
         .then (res => {console.log(res); this.setState({users: res.data})})
         .catch(err => console.log(err, 'something went wrong'))
 
-        
+
+
+
       }
 
     handleClick = (e) => {
@@ -40,17 +45,17 @@ class Profile extends Component {
          axios({
             method: 'get',
             url: 'http://localhost:5000/auth/logout',
-            
+
         })
         .then (res => {console.log(res, 'Farewell, friend') })
         .then(alert('Hope to see you soon!'))
         .catch (err => console.log(err))
-         
+
         localStorage.clear();
 
         this.props.history.push('/')
 
-        
+
     }
 
     getFavouriteById () {
@@ -64,35 +69,35 @@ class Profile extends Component {
       }
 
       getAllFavourites () {
-        if (this.state.users != null) {
-        let itins = [];
-        let ids = this.state.users.favourites
-        console.log ('ids', ids)
-        axios.get('http://localhost:5000/itineraries/all')
-        .then (res => {itins.push(...res.data); console.log(res.data)})
-        .catch(err => console.log('fuck, man', err))
-        console.log('itinerarios', itins)
+          let favs = [];
+          if (this.state.users != null) {
+            let ids = this.state.users.favourites
+              if (ids) {
 
-        
-        let favs = "";
-        
-        for (let i = 0; i < itins.length; i++) {
-           favs= itins.filter(itin => itin._id[i].indexOf(ids))
-            
+                  ids.forEach(id => {
+                    axios.get ('http://localhost:5000/itineraries/go/' + id)
+                    .then(res => {console.log('favourites coming right away', res.status); favs.push(res.data)})
+                  });
+
+                  }
+              }
+              console.log('favs', favs);
+              return favs;
         }
-        
-        
-    }
-      }
 
-   
+
+
+
     render () {
         console.log ('state', this.state)
-        let users = this.state.users; 
+
+        // this.getFavouriteById()
+
+        let users = this.state.users;
         let user = users != null ? (
             <div className = 'perfil'>
             <div>
-                <img src = {users.picture} alt='Cannot see your face from here' />                
+                <img src = {users.picture} alt='Cannot see your face from here' />
             </div>
             <div className = 'info'>
                 <h3 className = 'donde center'>Welcome, {users.first_name}. Nice to see you again.</h3>
@@ -102,7 +107,7 @@ class Profile extends Component {
                     <li>Name: {users.first_name + ' ' + users.last_name}</li>
                     <li>Age: {users.age}</li>
                     <li>Mail: {users.mail}</li>
-                    
+
                 </ul>
                 </div>
             </div>
@@ -125,30 +130,29 @@ class Profile extends Component {
             </div>
         );
 
-   
+
+
+        let favourites = this.getAllFavourites();
+
         
-        this.getFavouriteById()
-        
-        let {favourites} = this.state;
-                   
         let favouritesList = favourites.length ? (
             favourites.map( favourite => {
                 return (
                     <div className= 'center'>
-          <h4 className='donde'>You love these itineraries: </h4>
-          
-        <div className="user-card ">
-          <div>
-            <img src = {favourite.image} alt = 'do not remember that face' /> 
-          </div> 
-          
-          <div className = 'user-content'>
-            <h4 className="red-text">{favourite.name}</h4>
-            <p>{favourite.city}</p>                
-           
-          </div>
-          </div>
-          </div>
+                      <h4 className='donde'>You love these itineraries: </h4>
+
+                      <div className="user-card ">
+                      <div>
+                        <img src = {favourite.image} alt = 'do not remember that face' />
+                      </div>
+
+                      <div className = 'user-content'>
+                        <h4 className="red-text">{favourite.name}</h4>
+                        <p>{favourite.city}</p>
+
+                      </div>
+                      </div>
+                    </div>
                 )
                 })
         ) : (
@@ -156,7 +160,7 @@ class Profile extends Component {
             <p className='donde'>You don't have any favourites yet.</p>
             </div>
         );
-        
+
         return (
             <div>
             <Navbar2 />
@@ -164,7 +168,7 @@ class Profile extends Component {
             <div>
                 <h2 id= 'who'>Profile</h2>
             </div>
-                
+
             <div >
                 {user}
             </div>
